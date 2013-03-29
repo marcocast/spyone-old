8fada4f33157170278d5640f6653500f56aff6d6
marcocast <marcocast@gmail.com>
2013-03-29 15:05:00 +0000

    spyone layout

---
+++ spyone/src/main/java/com/spyone/SpyoneApplication.java
@@ -0,0 +1,28 @@
+package com.spyone;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+@SuppressWarnings("restriction")
+public class SpyoneApplication extends Application {
+      
+    private Parent root;
+
+    @Override public void init() throws Exception {       
+        root = FXMLLoader.load(SpyoneApplication.class.getResource("/UI/spyone.fxml"));
+    }
+    
+    @Override public void start(Stage stage) throws Exception {
+    	Scene preloaderScene = new Scene(root,1200,700);
+        preloaderScene.getStylesheets().add(
+        		SpyoneApplication.class.getResource("/UI/spyone.css").toExternalForm());
+        stage.setScene(preloaderScene);
+        stage.show();
+        
+    }    
+    public static void main(String[] args) { launch(args); }
+}
